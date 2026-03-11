9c5df3dbd161d60276fb7bb98b6230a1df21d38a
A1exKonst <94910859+A1exKonst@users.noreply.github.com>
2026-03-11 15:09:30 +0300

    add Shape test

--- hw_tensor_compilers/tests/test_main.cpp
+++ hw_tensor_compilers/tests/test_main.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include "graph/graph.h"
 
 // Простейший тест на проверку математики
 TEST(BasicAssertions, MathCheck) {
@@ -9,4 +10,16 @@ TEST(BasicAssertions, MathCheck) {
 TEST(ProjectLogic, SimpleTrue) {
     bool is_compiler_working = true;
     ASSERT_TRUE(is_compiler_working);
+}
+
+TEST(GraphEngine, ShapeInitialization) {
+    graph_engine::Shape shape = graph_engine::Shape();
+    ASSERT_EQ(shape.rank(), 0) << "ERR: rank after empty initialization is not 0";
+    /*
+    bool is_zero = true;
+    for (int i = 0; i < graph_engine::MAX_VALUE_RANK; ++i) {
+        is_zero = is_zero * shape[i];
+    }
+    EXPECT_EQ(is_zero, false) << "ERR: shape initialized with non-zero dims";
+    */
 }
